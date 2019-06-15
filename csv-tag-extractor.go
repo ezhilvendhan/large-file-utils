98738375c74ba0748469e57508b78870be933217
Ezhil Vendhan <uibuddha@gmail.com>
2019-06-15 22:37:24 +0800

    Add loggers

--- csv-tag-extractor.go
+++ csv-tag-extractor.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-  "fmt"
   "os"
   "log"
   "strconv"
@@ -12,10 +11,12 @@ import (
   "io/ioutil"
   "bufio"
   "time"
+  "sync"
 )
 
 const Out = "out"
 var tags = make(map[string]bool)
+var wg sync.WaitGroup
 
 func main() {
   start := time.Now()
@@ -29,22 +30,24 @@ func main() {
     folderPath := filepath.Join(cwd, strconv.Itoa(i))
     if _, err := os.Stat(folderPath); err == nil {
       if os.IsNotExist(err) {
-        fmt.Printf("stat error [%v]\n", err)
+        log.Printf("stat error [%v]\n", err)
         break
       }
-      fmt.Printf(folderPath+"\n")
+      log.Printf(folderPath+"\n")
       wErr := filepath.Walk(folderPath, func(path string, info os.FileInfo, wErr error) error {
         if info.IsDir() {
           return nil
         }
         if filepath.Ext(path) == ".csv" {
           tags = make(map[string]bool)
-          ProcessFile(path)
+          wg.Add(1)
+          go ProcessFile(path)
+          wg.Wait()
         }
         return nil
       })
       if wErr != nil {
-        fmt.Printf("walk error [%v]\n", wErr)
+        log.Printf("walk error [%v]\n", wErr)
       }
     }
   }
@@ -62,6 +65,11 @@ func getCWD() (_path string){
 }
 
 func ProcessFile(_file string) {
+  defer wg.Done()
+  log.Printf("---------------------------")
+  log.Printf("Memory Usage Pre-processing")
+  PrintMemUsage()
+  log.Printf("Now processing %s", _file)
   file, err := os.Open(_file)
   if err != nil {
       log.Fatal(err)
@@ -71,27 +79,28 @@ func ProcessFile(_file string) {
   dataToWrite := ""
   prevTag := ""
   scanner := bufio.NewScanner(file)
+  totalLines := 0
+  extractedLines := 0
+  lineForThisTag := 0
   for scanner.Scan() {
+    totalLines++
     line := scanner.Text()
     _tag := strings.Split(line, ",")[0]
     line = line+"\n"
     if tags[_tag] == true {
       dataToWrite += line
+      lineForThisTag++
     } else {
       tags[_tag] = true
       if prevTag != "" {
         _tagFileName := filepath.Join(getCWD(), "out", prevTag+".csv")
-        if(!Exists(_tagFileName)) {
-          _err := ioutil.WriteFile(_tagFileName, []byte(dataToWrite), 0777)
-          if _err != nil {
-            fmt.Printf("Unable to write file: %v", _err)
-          }
-        } else {
-          AppendFile(_tagFileName, dataToWrite)
-        }
+        AppendOrCreateFile(_tagFileName, dataToWrite)
+        log.Printf("Tag [%s] has [%d] records", prevTag, lineForThisTag)
+        extractedLines += lineForThisTag
       }
       dataToWrite = line
       prevTag = _tag
+      lineForThisTag = 1
     }
   }
 
@@ -100,14 +109,23 @@ func ProcessFile(_file string) {
   }
 
   _tagFileName := filepath.Join(getCWD(), "out", prevTag+".csv")
+  log.Printf("Tag [%s] has [%d] records", prevTag, lineForThisTag)
+  extractedLines += lineForThisTag
   if(!Exists(_tagFileName)) {
     _err := ioutil.WriteFile(_tagFileName, []byte(dataToWrite), 0777)
     if _err != nil {
-      fmt.Printf("Unable to write file: %v", _err)
+      log.Printf("Unable to write file: %v", _err)
     }
   } else {
-    AppendFile(_tagFileName, dataToWrite)
+    AppendOrCreateFile(_tagFileName, dataToWrite)
   }
+  log.Printf("Total records : %d", totalLines)
+  log.Printf("Extracted records : %d", extractedLines)
+  log.Printf("Processing DONE \n")
+  log.Printf("Memory Usage Post-processing")
+  PrintMemUsage()
+  log.Printf("---------------------------")
+  runtime.GC()
 }
 
 func Exists(name string) bool {
@@ -119,8 +137,8 @@ func Exists(name string) bool {
   return true
 }
 
-func AppendFile(fileName string, line string) {     
-  file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND, 0644)
+func AppendOrCreateFile(fileName string, line string) {     
+  file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
   if err != nil {
     log.Fatalf("failed opening file: %s", err)
   }
@@ -130,4 +148,17 @@ func AppendFile(fileName string, line string) {
   if err != nil {
       log.Fatalf("failed writing to file: %s", err)
   }
+}
+
+func PrintMemUsage() {
+  var m runtime.MemStats
+  runtime.ReadMemStats(&m)
+  log.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+  log.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+  log.Printf("\tSys = %v MiB", bToMb(m.Sys))
+  log.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+  return b / 1024 / 1024
 }
